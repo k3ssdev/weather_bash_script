56f93acf9c4e4065613800f2c11764056005cb6a
Alberto Pérez <63558530+k3ssdev@users.noreply.github.com>
2023-04-23 19:47:30 +0200

    Update weather-bar.sh

--- weather-bar.sh
+++ weather-bar.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Obtener la información del clima desde wttr.in
+# Get weather information from wttr.in.
 weather=$(curl -sS "wttr.in/?format=%C+%t+%h")
 
-# Extraer la condición meteorológica, la temperatura y la humedad utilizando expresiones regulares
+# Extract the weather condition, temperature, and humidity using regular expressions.
 condition=$(echo "$weather" | grep -oE '^[[:alpha:] ]+[[:alpha:]]')
 temperature=$(echo "$weather" | grep -oE '[+-]?[0-9]+°C')
 humidity=$(echo "$weather" | grep -oE '[0-9]+%')
 
-# Seleccionar el icono adecuado según la condición meteorológica
+# Select the appropriate icon according to the weather condition.
 case $condition in
-  "Clear"*) icon="";;
-  "Partly cloudy"*) icon="";;
-  "Cloudy"*) icon="";;
-  "Mist"*) icon="";;
-  "Fog"*) icon="";;
-  "Light rain"*) icon="";;
-  "Moderate rain"*) icon="";;
-  "Heavy rain"*) icon="";;
-  "Overcast"*) icon="";;
-  *) icon="";;
+  "Clear"*) icon=" ";; #\uf305
+  "Partly cloudy"*) icon=" ";; #\uf002
+  "Cloudy"*) icon=" ";; #\uf041
+  "Mist"*) icon=" ";; #\uf74e
+  "Fog"*) icon=" ";; #uf74e
+  "Light rain"*) icon=" ";; #\ue009
+  "Moderate rain"*) icon=" ";; #\ue009
+  "Heavy rain"*) icon=" ";; #\uf02d
+  "Overcast"*) icon=" ";; #\uf041
+  *) icon=" ";; #\u2757
 esac
 
-# Imprimir la información del clima
+# Print the weather information.
 echo "$icon $condition $temperature $humidity"
